3343e40690793a59aede390b5833a5794365c3a7
Niroshian Balasubramaniam <niroshian.b@gmail.com>
2021-04-10 22:11:10 -0400

    fixed background slideshow

--- client/src/Components/Landing.js
+++ client/src/Components/Landing.js
@@ -14,34 +14,28 @@ const Landing = () => {
 
 const BGfade = keyframes`
     0%{
-        background-image: url('../../assets/landingImages/5-by-jakub-kapusnak.jpg');
+        background-image: url('../../assets/landingImages/4-by-jakub-kapusnak.jpg'), url('../../assets/landingImages/1-by-jeswin-thomas.jpg');
     }
-    20% {
-        background-image: url("../../assets/landingImages/1-by-markus-winkler.jpg");
+    12% {
+        background-image: url('../../assets/landingImages/1-by-jeswin-thomas.jpg'), ;
     }
-    20%{
-        background-image: url("../../assets/landingImages/1-by-markus-winkler.jpg");
-    }
-    30% {
-        background-image: url('../../assets/landingImages/2-by-jeswin-thomas.jpg');
-    }
-    40%{
-        background-image: url('../../assets/landingImages/2-by-jeswin-thomas.jpg');
+    25%{
+        background-image: url('../../assets/landingImages/1-by-jeswin-thomas.jpg'), url('../../assets/landingImages/2-by-jing-xi-lau.jpg');
     }
     50%{
-        background-image: url('../../assets/landingImages/3-by-jing-xi-lau.jpg');
+        background-image: url('../../assets/landingImages/2-by-jing-xi-lau.jpg');
     }
-    60%{
-        background-image: url('../../assets/landingImages/3-by-jing-xi-lau.jpg');
+    62%{
+        background-image: url('../../assets/landingImages/2-by-jing-xi-lau.jpg'), url('../../assets/landingImages/3-by-spencer-davis.jpg');
     }
-    70%{
-        background-image: url('../../assets/landingImages/4-by-spencer-davis.jpg');
+    75%{
+        background-image: url('../../assets/landingImages/3-by-spencer-davis.jpg');
     }
-    80%{
-        background-image: url('../../assets/landingImages/4-by-spencer-davis.jpg');
+    87%{
+        background-image: url('../../assets/landingImages/3-by-spencer-davis.jpg'), url('../../assets/landingImages/4-by-jakub-kapusnak.jpg');
     }
     100% {
-        background-image: url('../../assets/landingImages/5-by-jakub-kapusnak.jpg');
+        background-image: url('../../assets/landingImages/4-by-jakub-kapusnak.jpg');
     }
 `;
 
@@ -49,12 +43,13 @@ const Wrapper = styled.div`
 	width: 100vw;
 	height: 100vh;
 
-	background-image: url('../../assets/landingImages/1-by-markus-winkler.jpg');
+	background-image: url('../../assets/landingImages/4-by-jakub-kapusnak.jpg');
 	background-position: center center;
 	background-size: cover;
 	background-repeat: no-repeat;
+	backface-visibility: none;
 
-	animation: ${BGfade} 60s linear infinite 0s;
+	animation: ${BGfade} 30s linear infinite 0s;
 	animation-timing-function: ease-in-out;
 
 	display: flex;
